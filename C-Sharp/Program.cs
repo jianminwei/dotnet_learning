0b83f154d1203ec96e84f8aff516e8de4ceef657
Jianmin Wei <jianminwei1@gmail.com>
2023-03-23 13:48:58 -0400

    Initial commit after http client posting region

---
+++ C-Sharp/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Text.Json;
+
+namespace dcf_client
+{
+
+    class Region
+    {
+        public long id { get; set; }
+        public string name { get; set; } = "";
+    }
+
+    class DcfApiTest
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:8080/api/regions");
+            var content = new StringContent("{ \"name\": \"North East Region\" }", null, "application/json");
+            request.Content = content;
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var result = response.Content.ReadAsStringAsync().Result;
+
+            Region? region = JsonSerializer.Deserialize<Region>(result);
+
+            if (region is not null)
+            {
+                System.Console.WriteLine(region.id + "," + region.name);
+            }
+
+        }
+    }
+
+}
---
+++ console-test1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+class Test
+{
+
+    public static void Main(string[] args)
+    {
+        int x = 10;
+        int y = 20;
+
+        int tmp = 0;
+
+        tmp = x;
+        x = y;
+        y = tmp;
+
+        System.Console.WriteLine(x);
+        System.Console.WriteLine(y);
+
+
+    }
+}
